b55259ebcd0f7771689ee9f2f6e0fa52a13e475c
lucasmathcode <164958181+lucasmathcode@users.noreply.github.com>
2024-09-26 14:31:23 -0300

    Add files via upload

---
+++ Atividade Complementar/app.js
@@ -0,0 +1,31 @@
+console.log("Olá Mundo");
+
+function pesquisar() {
+  const campoPesquisa = document
+    .getElementById("campo-pesquisa")
+    .value.toLowerCase();
+  const section = document.getElementById("resultados-pesquisa");
+
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Por favor, digite algo para pesquisar.</p>";
+    return;
+  }
+
+  let resultados = "";
+  for (let dado of dados) {
+    if (
+      dado.titulo.toLowerCase().includes(campoPesquisa) ||
+      dado.descricao.toLowerCase().includes(campoPesquisa) ||
+      dado.tags.includes(campoPesquisa)
+    ) {
+      resultados += `
+                <div class="item-resultado">
+                    <h2><a href="${dado.link}" target="_blank">${dado.titulo}</a></h2>
+                    <p>${dado.descricao}</p>
+                </div>
+            `;
+    }
+  }
+
+  section.innerHTML = resultados || "<p>Nada foi encontrado.</p>";
+}
---
+++ Atividade Complementar/dados.js
@@ -0,0 +1,20 @@
+const dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao: "A skatista brasileira conhecida como 'Fadinha'.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada olimpíadas",
+  },
+  {
+    titulo: "Gabriel Medina",
+    descricao: "Surfista brasileiro, bicampeão mundial de surfe.",
+    link: "https://pt.wikipedia.org/wiki/Gabriel_Medina",
+    tags: "surfe campeão olimpíadas",
+  },
+  {
+    titulo: "Arthur Zanetti",
+    descricao: "Ginasta brasileiro e campeão olímpico nas argolas.",
+    link: "https://pt.wikipedia.org/wiki/Arthur_Zanetti",
+    tags: "ginástica argolas olimpíadas",
+  },
+];
